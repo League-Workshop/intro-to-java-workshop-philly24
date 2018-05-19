db30d300271f7346f048ad262c6c3b00e74cd6c1
league <league@imac-6.attlocal.net>
2018-05-19 12:33:22 -0700

    day 1

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,6 +1,9 @@
 package section2;
 
 import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
@@ -8,8 +11,21 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	System.out.println("hello world");
+		
+		
+	Robot arnold= new Robot();
+	arnold.setSpeed(100);
+
+	arnold.move(40);
+	arnold.turn(90);
+	arnold.move(40);
+	arnold.turn(90);
+	arnold.move(40);
+	arnold.turn(90);
+	arnold.move(40);
+	arnold
+    
 	
-		
-		
 	}
 }
